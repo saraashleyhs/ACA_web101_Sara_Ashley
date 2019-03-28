b55af9d8b9f831803fd9622568a1e90f6f2761bb
saraashleyhs <sara.ashleyhs@gmail.com>
2019-03-27 19:15:07 -0500

    Finished TicTacToe

--- myPortfolio/scripts/TicTacToe.js
+++ myPortfolio/scripts/TicTacToe.js
@@ -5,4 +5,21 @@
 //Player clicks on a spot on the board, if void then mark if not then give an error. 
 
 //
+var gameMarker = "X";
+
+function changeMarkerToX(){ 
+    gameMarker = "X";
+    //console.log("The X button was clicked!");
+} 
+
+
+function changeMarkerToO(){ 
+    gameMarker = "O";
+    //console.log("The O button was clicked!");
+}
+
+function placeMark(divId){ 
+    var place = document.getElementById(divId);
+    place.innerHTML = gameMarker;
+}
 

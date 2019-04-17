42d9e260f34500be3eef0a328603566cf989fec7
saraashleyhs <sara.ashleyhs@gmail.com>
2019-04-17 16:34:14 -0500

    Dashboard push

--- myPortfolio/scripts/Dashboard.js
+++ myPortfolio/scripts/Dashboard.js
@@ -31,3 +31,46 @@ function getLocation() {
 function displayPosition(position) {
   myLocation.innerHTML = "Longitude: " + position.coords.longitude + " Latitude: " + position.coords.latitude
 }
+
+//Calculator Functions
+function displayNumber(num) {
+  document.querySelector("#results").value += num;
+}
+
+function clearResults() {
+  document.querySelector("#results").value = "";
+}
+
+function addition() {
+  document.querySelector("#results").value += "+";
+}
+
+function subtraction() {
+  document.querySelector("#results").value += "-";
+}
+
+function division() {
+  document.querySelector("#results").value += "/";
+}
+
+function multiply() {
+  document.querySelector("#results").value += "*";
+}
+//Tried to use toggle for changing the sign..??
+//function signToggle() {
+//  $("#signbutton").toggle
+//document.querySelector("#results").value += cuurent.slice();
+//}
+
+function signFunction() {
+  document.querySelector("#results").value = document.querySelector("#results").value * -1;
+}
+
+function equals() {
+  document.querySelector("#results").value =      eval(document.querySelector("#results").value);
+}
+
+function deleteLast() {
+  let current = document.querySelector("#results").value;
+  document.querySelector("#results").value = current.slice(0, -1);
+}
